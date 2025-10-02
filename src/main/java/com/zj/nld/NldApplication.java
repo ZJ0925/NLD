6d2b830b162a6dbcb34d564d78942cd67798113a
YZJ <a9203335967@gmail.com>
2025-10-02 21:08:49 +0800

    safe

--- src/main/java/com/zj/nld/NldApplication.java
+++ src/main/java/com/zj/nld/NldApplication.java
@@ -9,11 +9,11 @@ import java.security.Security;
 public class NldApplication {
 
     //開發時註解
-//    static {
-//        // 移除 TLSv1 和 TLSv1.1 從禁用清單
-//        Security.setProperty("jdk.tls.disabledAlgorithms",
-//                "SSLv3, RC4, DES, MD5withRSA, DH keySize < 1024, EC keySize < 224, 3DES_EDE_CBC, anon, NULL");
-//    }
+    static {
+        // 移除 TLSv1 和 TLSv1.1 從禁用清單
+        Security.setProperty("jdk.tls.disabledAlgorithms",
+                "SSLv3, RC4, DES, MD5withRSA, DH keySize < 1024, EC keySize < 224, 3DES_EDE_CBC, anon, NULL");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NldApplication.class, args);
